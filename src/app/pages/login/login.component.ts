cb87c2fb25aa0ba79cdbce4532ad6a711dc1b511
Bonevy BEBY <bonevybeby@gmail.com>
2024-08-27 08:44:47 +0200

    added: create an API and an object

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+ loginObj:any={
+  "EmailId": "",
+  "Password": ""
+}
 }
