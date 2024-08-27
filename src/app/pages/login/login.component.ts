4763e35504d77b3248e8bf48b02af1f8d9070251
Bonevy BEBY <bonevybeby@gmail.com>
2024-08-27 09:16:17 +0200

    added: router//navigation Succeded

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -15,12 +16,17 @@ export class LoginComponent {
   "Password": ""
  }
  http=inject(HttpClient);
+
+ constructor(private router:Router){
+
+ }
  onLogin(){
   debugger;
    this.http.post("https://freeapi.miniprojectideas.com/api/User/Login",this.loginObj).subscribe((res:any)=>{
     debugger;
      if(res.result){
        alert("Login Success");
+       this.router.navigateByUrl("dashboard");
      }else{
       alert("Check User Name or Password!");
      }
